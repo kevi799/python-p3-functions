0769b699d010a117e564330542e346f6bf22e79a
kelvinMulinge <kelvinmulinge9702@gmail.com>
2024-11-29 19:58:58 +0300

    Working with Functios

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+    
+greet_programmer()    
 
 def greet(name):
-    pass
+   
+   print(f'Hello, {name}!')
+
+greet('Kevin')
 
 def greet_with_default(name="programmer"):
-    pass
+    
+    print(f'Hello, {name}!')
+
+greet_with_default()    
 
 def add(num1, num2):
-    pass
+    sum = num1 +num2
+    return sum
 
 def halve(number):
-    pass
+    result = number / 2
+    return result
+halve(9)
